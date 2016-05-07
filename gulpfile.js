6a5dae048306e7c5b2c141f45a035eb4877c3699
Ollie Reardon <ojreardon@gmail.com>
2016-05-07 15:28:24 +0100

    Blank setup for testing latgest merge request

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+var gulp = require('gulp'),
+zip = require('gulp-zip'),
+watch = require('gulp-watch'),
+jshint = require('gulp-jshint'),
+gp_concat = require('gulp-concat'),
+gp_uglify = require('gulp-uglify'),
+imageop = require('gulp-image-optimization'),
+minifyHTML = require('gulp-minify-html'),
+rename = require('gulp-rename');
+
+var zip_files = ['build/game.js', 'build/index.html'], //Files to be added to the zip folder use "<directory goes here>/*" for all files inside the directory
+	js_files = ['game.js', 'convergame-engine/convergame.js', 'scenes/*', 'objects/*'], //All your JS files to be combined and minified
+	img_files = ['assets/*.png','assets/*.jpg','src/**/*.gif','assets/*.jpeg'];
+
+//Zip up the JS/HTML required for the game
+gulp.task('zip', function () {
+    return gulp.src(zip_files, {base: "."})
+        .pipe(zip('game.zip'))
+        .pipe(gulp.dest('build'));
+});
+
+
+//Run this task once the game is ready to ship!
+gulp.task('publish', ['build-js', 'build-html', 'zip']);
+
+//Compress Images
+//Todo: Test to see if it'll override images
+gulp.task('images', function(cb) {
+    gulp.src(img_files).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('assets')).on('end', cb).on('error', cb);
+});
+
+//Minify the HTML
+gulp.task('build-html', function() { 
+  return gulp.src('./index.unmin.html')
+    .pipe(minifyHTML())
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('./build/'));
+});
+
+//Build the JS and minify
+gulp.task('build-js', function() {
+	return gulp.src(js_files)
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(gp_uglify())
+		.pipe(gp_concat('game.js'))
+		.pipe(gulp.dest('./build/'));
+});
+
+//Build the JS without minifying
+gulp.task('build-dev', function() {
+	return gulp.src(js_files)
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(gp_concat('game.js'))
+		.pipe(gulp.dest('./build/'));
+});
+
+//Watch Task
+gulp.task('watch', function() {
+    gulp.watch('./index.unmin.html', ['build-html']);
+    gulp.watch(js_dir, ['js-build-dev', 'build-dev']);
+});
+
+//Default Task
+gulp.task('default', ['build-html', 'build-dev']);
+
+
+//Legacy 'build' alias for build-dev task
+gulp.task('build', ['build-dev']);
